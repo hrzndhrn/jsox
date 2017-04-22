4edc04346cbc10d89e23aaf83c1a3bd61a06fa4b
Marcus Kruse <esurk.sucram@gmail.com>
2017-04-22 07:50:03 +0200

    Create project

---
+++ lib/jsox.ex
@@ -0,0 +1,18 @@
+defmodule Jsox do
+  @moduledoc """
+  Documentation for Jsox.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Jsox.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/jsox_test.exs
@@ -0,0 +1,8 @@
+defmodule JsoxTest do
+  use ExUnit.Case
+  doctest Jsox
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end

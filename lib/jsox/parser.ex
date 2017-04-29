8ae6a67cbfaba134929bfc1b0ab137cddddbcc1b
Marcus Kruse <esurk.sucram@gmail.com>
2017-04-29 12:51:38 +0200

    Rename iodata to data

--- lib/jsox/parser.ex
+++ lib/jsox/parser.ex
@@ -41,9 +41,9 @@ defmodule Jsox.Parser do
   @comma ?,
 
   @spec parse(iodata) :: {:ok, json} | {:error, String.t}
-  def parse(iodata) do
-    {result, iodata, pos} = parse_json(iodata, 0)
-    if iodata =~ ~r/^\s*$/ do
+  def parse(data) do
+    {result, data, pos} = parse_json(data, 0)
+    if data =~ ~r/^\s*$/ do
       {:ok, result}
     else
       {:error, :eof, pos}
@@ -52,165 +52,165 @@ defmodule Jsox.Parser do
     {token, pos} -> {:error, token, pos}
   end
 
-  defp parse_json(<<char>> <> iodata, pos) do
+  defp parse_json(<<char>> <> data, pos) do
     pos = pos + 1
     cond do
-      char in @whitespace -> parse_json(iodata, pos)
-      char == @quotation_mark -> parse_string(iodata, pos, [])
-      char == @left_square_bracket -> parse_list(iodata, pos, [])
-      char == @left_curly_bracket-> parse_map(iodata, pos, [])
-      char == @minus_sign or char in @digits -> parse_number(iodata, pos, [char])
-      char == ?t -> parse_true(iodata, pos)
-      char == ?f -> parse_false(iodata, pos)
-      char == ?n -> parse_null(iodata, pos)
+      char in @whitespace -> parse_json(data, pos)
+      char == @quotation_mark -> parse_string(data, pos, [])
+      char == @left_square_bracket -> parse_list(data, pos, [])
+      char == @left_curly_bracket-> parse_map(data, pos, [])
+      char == @minus_sign or char in @digits -> parse_number(data, pos, [char])
+      char == ?t -> parse_true(data, pos)
+      char == ?f -> parse_false(data, pos)
+      char == ?n -> parse_null(data, pos)
       true -> throw {:json, pos}
     end
   end
 
-  defp parse_true(<<"rue">> <> iodata, pos), do: {true, iodata, pos + 3}
-  defp parse_true(_iodata, pos), do: throw {:true, pos}
+  defp parse_true(<<"rue">> <> data, pos), do: {true, data, pos + 3}
+  defp parse_true(_data, pos), do: throw {:true, pos}
 
-  defp parse_false(<<"alse">> <> iodata, pos), do: {false, iodata, pos + 4}
-  defp parse_false(_iodata, pos), do: throw {:false, pos}
+  defp parse_false(<<"alse">> <> data, pos), do: {false, data, pos + 4}
+  defp parse_false(_data, pos), do: throw {:false, pos}
 
-  defp parse_null(<<"ull">> <> iodata, pos), do: {nil, iodata, pos + 3}
-  defp parse_null(_iodata, pos), do: throw {:null, pos}
+  defp parse_null(<<"ull">> <> data, pos), do: {nil, data, pos + 3}
+  defp parse_null(_data, pos), do: throw {:null, pos}
 
-  defp parse_key(<<char>> <> iodata, pos) do
+  defp parse_key(<<char>> <> data, pos) do
     pos = pos + 1
     cond do
-      char in @whitespace -> parse_key(iodata, pos)
-      char == @quotation_mark -> parse_string(iodata, pos, [])
+      char in @whitespace -> parse_key(data, pos)
+      char == @quotation_mark -> parse_string(data, pos, [])
       true -> throw {:key, pos}
     end
   end
 
-  defp parse_number(<<char>> <> iodata, pos, chars)
+  defp parse_number(<<char>> <> data, pos, chars)
     when char in @digits,
-    do: parse_number(iodata, pos + 1, [char|chars])
-  defp parse_number(<<@full_stop>> <> _iodata, pos, [@minus_sign]),
+    do: parse_number(data, pos + 1, [char|chars])
+  defp parse_number(<<@full_stop>> <> _data, pos, [@minus_sign]),
     do: throw {:number, pos}
-  defp parse_number(<<@full_stop>> <> iodata, pos, chars),
-    do: parse_float(iodata, pos + 1, [@full_stop|chars])
-  defp parse_number(<<char>> <> _iodata, pos, [@minus_sign])
+  defp parse_number(<<@full_stop>> <> data, pos, chars),
+    do: parse_float(data, pos + 1, [@full_stop|chars])
+  defp parse_number(<<char>> <> _data, pos, [@minus_sign])
     when char in @exps,
     do: throw {:number, pos}
-  defp parse_number(<<char>> <> iodata, pos, chars)
+  defp parse_number(<<char>> <> data, pos, chars)
     when char in @exps,
-    do: parse_exponential(iodata, pos + 1, [@exp|['.0'|chars]])
-  defp parse_number(_iodata, pos, [@minus_sign]),
+    do: parse_exponential(data, pos + 1, [@exp|['.0'|chars]])
+  defp parse_number(_data, pos, [@minus_sign]),
     do: throw {:number, pos}
-  defp parse_number(iodata, pos, chars) do
+  defp parse_number(data, pos, chars) do
     result = chars
              |> Enum.reverse
              |> IO.iodata_to_binary
              |> String.to_integer
-    {result, iodata, pos}
+    {result, data, pos}
   end
 
-  defp parse_exponential(<<@minus_sign>> <> iodata, pos, [@exp|_] = chars),
-    do: parse_exponential(iodata, pos + 1, [@minus_sign|chars])
-  defp parse_exponential(<<char>> <> iodata, pos, chars)
+  defp parse_exponential(<<@minus_sign>> <> data, pos, [@exp|_] = chars),
+    do: parse_exponential(data, pos + 1, [@minus_sign|chars])
+  defp parse_exponential(<<char>> <> data, pos, chars)
     when char in @digits,
-    do: parse_exponential(iodata, pos + 1, [char|chars])
-  defp parse_exponential(_iodata, pos, [@exp|_]),
+    do: parse_exponential(data, pos + 1, [char|chars])
+  defp parse_exponential(_data, pos, [@exp|_]),
     do: throw {:exponential, pos}
-  defp parse_exponential(_iodata, pos, [@minus_sign|_]),
+  defp parse_exponential(_data, pos, [@minus_sign|_]),
     do: throw {:exponential, pos}
-  defp parse_exponential(iodata, pos, chars) do
+  defp parse_exponential(data, pos, chars) do
     result = chars
         |> Enum.reverse
         |> IO.iodata_to_binary
         |> String.to_float
-    {result, iodata, pos}
+    {result, data, pos}
   end
 
-  defp parse_float(<<char>> <> _iodata, pos, [@full_stop|_])
+  defp parse_float(<<char>> <> _data, pos, [@full_stop|_])
     when char in @exps,
     do: throw {:float, pos}
-  defp parse_float(<<char>> <> iodata, pos, chars)
+  defp parse_float(<<char>> <> data, pos, chars)
     when char in @exps,
-    do: parse_exponential(iodata, pos + 1, [@exp|chars])
-  defp parse_float(<<char>> <> iodata, pos, chars)
+    do: parse_exponential(data, pos + 1, [@exp|chars])
+  defp parse_float(<<char>> <> data, pos, chars)
     when char in @digits,
-    do: parse_float(iodata, pos + 1, [char|chars])
-  defp parse_float(iodata, pos, chars) do
+    do: parse_float(data, pos + 1, [char|chars])
+  defp parse_float(data, pos, chars) do
     result = chars
              |> Enum.reverse
              |> IO.iodata_to_binary
              |> String.to_float
-    {result, iodata, pos}
+    {result, data, pos}
   end
 
-  defp parse_string(<<@quotation_mark>> <> iodata, pos, chars) do
+  defp parse_string(<<@quotation_mark>> <> data, pos, chars) do
     result = chars
              |> Enum.reverse
              |> IO.iodata_to_binary
-    {result, iodata, pos}
+    {result, data, pos}
   end
-  defp parse_string(<<@escape, @solidus>> <> iodata, pos, chars),
-    do: parse_string(iodata, pos + 2, [@solidus|chars])
-  defp parse_string(<<@escape, char>> <> iodata, pos, chars)
+  defp parse_string(<<@escape, @solidus>> <> data, pos, chars),
+    do: parse_string(data, pos + 2, [@solidus|chars])
+  defp parse_string(<<@escape, char>> <> data, pos, chars)
     when char in @escape_chars,
-    do: parse_string(iodata, pos + 2, [Map.get(@escapes, char)|chars])
+    do: parse_string(data, pos + 2, [Map.get(@escapes, char)|chars])
   defp parse_string(
-             <<@escape, @unicode, a1, b1, c1, d1, @escape, @unicode, a2, b2, c2, d2>> <> iodata,
+             <<@escape, @unicode, a1, b1, c1, d1, @escape, @unicode, a2, b2, c2, d2>> <> data,
              pos, chars)
     when a1 in @surrogate_a and a2 in @surrogate_a and b1 in @surrogate_b1 and b2 in @surrogate_b2 do
     hi = List.to_integer([a1, b1, c1, d1], 16)
     lo = List.to_integer([a2, b2, c2, d2], 16)
     codepoint = 0x10000 + ((hi &&& 0x03FF) <<< 10) + (lo &&& 0x03FF)
-    parse_string(iodata, pos + 11, [<<codepoint :: utf8>>|chars])
+    parse_string(data, pos + 11, [<<codepoint :: utf8>>|chars])
   end
-  defp parse_string(<<@escape, @unicode, seq :: binary-size(4)>> <> iodata, pos, chars),
-    do: parse_string(iodata, pos + 6, [<<String.to_integer(seq, 16) :: utf8>>|chars])
-  defp parse_string(<<@escape>> <> _iodata, pos, _chars),
+  defp parse_string(<<@escape, @unicode, seq :: binary-size(4)>> <> data, pos, chars),
+    do: parse_string(data, pos + 6, [<<String.to_integer(seq, 16) :: utf8>>|chars])
+  defp parse_string(<<@escape>> <> _data, pos, _chars),
     do: throw {:string, pos}
-  defp parse_string(<<char>> <> iodata, pos, chars),
-    do: parse_string(iodata, pos + 1, [char|chars])
+  defp parse_string(<<char>> <> data, pos, chars),
+    do: parse_string(data, pos + 1, [char|chars])
 
-  defp parse_list(<<char>> <> iodata, pos, list)
+  defp parse_list(<<char>> <> data, pos, list)
     when char in @whitespace,
-    do: parse_list(iodata, pos + 1, list)
-  defp parse_list(<<@right_square_bracket>> <> iodata, pos, list),
-    do: {Enum.reverse(list), iodata, pos + 1}
-  defp parse_list(<<@comma>> <> _iodata, pos, []),
+    do: parse_list(data, pos + 1, list)
+  defp parse_list(<<@right_square_bracket>> <> data, pos, list),
+    do: {Enum.reverse(list), data, pos + 1}
+  defp parse_list(<<@comma>> <> _data, pos, []),
     do: throw {:list, pos}
-  defp parse_list(<<@comma>> <> iodata, pos, list) do
-    {result, iodata, pos} = parse_json(iodata, pos + 1)
-    parse_list(iodata, pos, [result|list])
+  defp parse_list(<<@comma>> <> data, pos, list) do
+    {result, data, pos} = parse_json(data, pos + 1)
+    parse_list(data, pos, [result|list])
   end
-  defp parse_list(iodata, pos, []) do
-    {result, iodata, pos} = parse_json(iodata, pos)
-    parse_list(iodata, pos, [result])
+  defp parse_list(data, pos, []) do
+    {result, data, pos} = parse_json(data, pos)
+    parse_list(data, pos, [result])
   end
-  defp parse_list(_iodata, pos, _list),
+  defp parse_list(_data, pos, _list),
     do: throw{:list, pos}
 
-  defp parse_map(<<char>> <> iodata, pos, list)
+  defp parse_map(<<char>> <> data, pos, list)
     when char in @whitespace,
-    do: parse_map(iodata,  pos + 1, list)
-  defp parse_map(<<@right_curly_bracket>> <> iodata, pos, list) do
+    do: parse_map(data,  pos + 1, list)
+  defp parse_map(<<@right_curly_bracket>> <> data, pos, list) do
     result = for [value, key] <- Enum.chunk(list, 2), into: %{}, do: {key, value}
-    {result, iodata, pos + 1}
+    {result, data, pos + 1}
   end
-  defp parse_map(<<@comma>> <> _iodata, pos, []),
+  defp parse_map(<<@comma>> <> _data, pos, []),
     do: throw {:map, pos + 1}
-  defp parse_map(<<@comma>> <> iodata, pos, list) do
-    {result, iodata, pos} = parse_key(iodata, pos + 1)
-    parse_map(iodata, pos + 1, [result|list])
+  defp parse_map(<<@comma>> <> data, pos, list) do
+    {result, data, pos} = parse_key(data, pos + 1)
+    parse_map(data, pos + 1, [result|list])
   end
-  defp parse_map(<<@colon>> <> _iodata, pos, []),
+  defp parse_map(<<@colon>> <> _data, pos, []),
     do: throw {:map, pos + 1}
-  defp parse_map(<<@colon>> <> iodata, pos, list) do
-    {result, iodata, pos} = parse_json(iodata, pos + 1)
-    parse_map(iodata, pos + 1, [result|list])
+  defp parse_map(<<@colon>> <> data, pos, list) do
+    {result, data, pos} = parse_json(data, pos + 1)
+    parse_map(data, pos + 1, [result|list])
   end
-  defp parse_map(iodata, pos, []) do
-    {result, iodata, pos} = parse_key(iodata, pos)
-    parse_map(iodata, pos, [result])
+  defp parse_map(data, pos, []) do
+    {result, data, pos} = parse_key(data, pos)
+    parse_map(data, pos, [result])
   end
-  defp parse_map(_iodata, pos, _list),
+  defp parse_map(_data, pos, _list),
     do: throw {:map, pos + 1}
 
 end

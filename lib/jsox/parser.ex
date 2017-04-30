874ad790fcac35c3f73ae57ae2a7c3b381534289
Marcus Kruse <esurk.sucram@gmail.com>
2017-04-30 12:46:14 +0200

    Clean up

--- lib/jsox/parser.ex
+++ lib/jsox/parser.ex
@@ -11,12 +11,8 @@ defmodule Jsox.Parser do
   @type json :: map | list | String.t | integer | float | true | false | nil
 
   @digits '0123456789'
-  @whitespace '\s\r\t\n'
-  @minus_sign ?-
-  @full_stop ?.
-  @exps 'eE'
-  @quotation_mark ?"
-  @escape ?\\
+  @minus_digits '-0123456789'
+  @whitespaces '\s\r\t\n'
   @escapes %{
     ?\\ => '\\',
     ?" => '\"',
@@ -28,15 +24,6 @@ defmodule Jsox.Parser do
     ?/ => '\/'
   }
   @escape_chars [?u|Map.keys(@escapes)]
-  @surrogate_a 'dD'
-  @surrogate_b1 '89abAb'
-  @surrogate_b2 'cedfCDEF'
-  @left_square_bracket ?[
-  @right_square_bracket ?]
-  @left_curly_bracket ?{
-  @right_curly_bracket ?}
-  @colon ?:
-  @comma ?,
 
   @spec parse(iodata) :: {:ok, json} | {:error, String.t}
   def parse(data) do
@@ -51,34 +38,34 @@ defmodule Jsox.Parser do
   end
 
   defp json(<<char>> <> data, pos)
-    when char in @whitespace,
+    when char in @whitespaces,
     do: json(data, pos + 1)
   defp json(<<char>> <> data, pos)
-    when char == @quotation_mark,
+    when char == ?",
     do: string(data, pos + 1, [])
   defp json(<<char>> <> data, pos)
-    when char == @left_square_bracket,
+    when char == ?[,
     do: list(data, pos + 1, [])
   defp json(<<char>> <> data, pos)
-    when char == @left_curly_bracket,
+    when char == ?{,
     do: map(data, pos + 1, [])
   defp json(<<char>> <> data, pos)
-    when char in '-0123456789',
+    when char in @minus_digits,
     do: number(data, pos + 1, [char], (if char == ?-, do: :minus, else: :digit))
-  defp json(<<"true">> <> data, pos),
+  defp json("true" <> data, pos),
     do: {true, data, pos + 4}
-  defp json(<<"false">> <> data, pos),
+  defp json("false" <> data, pos),
     do: {false, data, pos + 5}
-  defp json(<<"null">> <> data, pos),
+  defp json("null" <> data, pos),
     do: {nil, data, pos + 4}
   defp json(_data, pos),
     do: throw {:json, pos + 1}
 
   defp key(<<char>> <> data, pos)
-    when char in @whitespace,
+    when char in @whitespaces,
     do: key(data, pos + 1)
   defp key(<<char>> <> data, pos)
-    when char == @quotation_mark,
+    when char == ?",
     do: string(data, pos + 1, [])
   defp key(_data, pos),
     do: throw {:key, pos}
@@ -86,15 +73,15 @@ defmodule Jsox.Parser do
   defp number(<<char>> <> data, pos, chars, _last)
     when char in @digits,
     do: number(data, pos + 1, [chars,char], :digit)
-  defp number(<<@full_stop>> <> _data, pos, _chars, :minus),
+  defp number(<<?.>> <> _data, pos, _chars, :minus),
     do: throw {:number, pos + 1}
-  defp number(<<@full_stop>> <> data, pos, chars, _last),
-    do: float(data, pos + 1, [chars, @full_stop], :full_stop)
+  defp number(<<?.>> <> data, pos, chars, _last),
+    do: float(data, pos + 1, [chars, ?.], :full_stop)
   defp number(<<char>> <> _data, pos, _chars, :minus)
-    when char in @exps,
+    when char in 'eE',
     do: throw {:number, pos}
   defp number(<<char>> <> data, pos, chars, _last)
-    when char in @exps,
+    when char in 'eE',
     do: exponential(data, pos + 1, [chars, '.0e'], :exp)
   defp number(_data, pos, _chars, :minus),
     do: throw {:number, pos}
@@ -105,8 +92,8 @@ defmodule Jsox.Parser do
     {result, data, pos}
   end
 
-  defp exponential(<<@minus_sign>> <> data, pos, chars, :exp),
-    do: exponential(data, pos + 1, [chars, @minus_sign], :minus)
+  defp exponential(<<?->> <> data, pos, chars, :exp),
+    do: exponential(data, pos + 1, [chars, ?-], :minus)
   defp exponential(<<char>> <> data, pos, chars, _last)
     when char in @digits,
     do: exponential(data, pos + 1, [chars, char], :digit)
@@ -118,10 +105,10 @@ defmodule Jsox.Parser do
     do: {chars |> IO.iodata_to_binary |> String.to_float, data, pos}
 
   defp float(<<char>> <> _data, pos, _chars, :full_stop)
-    when char in @exps,
+    when char in 'eE',
     do: throw {:float, pos}
   defp float(<<char>> <> data, pos, chars, _last)
-    when char in @exps,
+    when char in 'eE',
     do: exponential(data, pos + 1, [chars, 'e'], :exp)
   defp float(<<char>> <> data, pos, chars, _last)
     when char in @digits,
@@ -129,7 +116,7 @@ defmodule Jsox.Parser do
   defp float(data, pos, chars, _last),
     do: {chars |> IO.iodata_to_binary |> String.to_float, data, pos}
 
-  defp string(<<@quotation_mark>> <> data, pos, chars),
+  defp string(<<?">> <> data, pos, chars),
     do: {chars |> Enum.reverse |> IO.iodata_to_binary, data, pos}
   defp string(<<?\\, char>> <> data, pos, chars) when char in @escape_chars do
     cond do
@@ -137,13 +124,13 @@ defmodule Jsox.Parser do
       true -> string(data, pos + 2, [Map.get(@escapes, char)|chars])
     end
   end
-  defp string(<<@escape>> <> _data, pos, _chars),
+  defp string(<<?\\>> <> _data, pos, _chars),
     do: throw {:string, pos}
   defp string(<<char>> <> data, pos, chars),
     do: string(data, pos + 1, [char|chars])
 
   defp unicode(<<a1, b1, c1, d1, ?\\, ?u, a2, b2, c2, d2>> <> data, pos, chars)
-    when a1 in @surrogate_a and a2 in @surrogate_a and b1 in @surrogate_b1 and b2 in @surrogate_b2 do
+    when a1 in 'dD' and a2 in 'dD' and b1 in '89abAb' and b2 in 'cedfCDEF' do
     hi = List.to_integer([a1, b1, c1, d1], 16)
     lo = List.to_integer([a2, b2, c2, d2], 16)
     codepoint = 0x10000 + ((hi &&& 0x03FF) <<< 10) + (lo &&& 0x03FF)
@@ -153,13 +140,13 @@ defmodule Jsox.Parser do
     do: string(data, pos + 6, [<<String.to_integer(seq, 16) :: utf8>>|chars])
 
   defp list(<<char>> <> data, pos, list)
-    when char in @whitespace,
+    when char in @whitespaces,
     do: list(data, pos + 1, list)
-  defp list(<<@right_square_bracket>> <> data, pos, list),
+  defp list(<<?]>> <> data, pos, list),
     do: {Enum.reverse(list), data, pos + 1}
-  defp list(<<@comma>> <> _data, pos, []),
+  defp list(<<?,>> <> _data, pos, []),
     do: throw {:list, pos}
-  defp list(<<@comma>> <> data, pos, list) do
+  defp list(<<?,>> <> data, pos, list) do
     {result, data, pos} = json(data, pos + 1)
     list(data, pos, [result|list])
   end
@@ -171,21 +158,21 @@ defmodule Jsox.Parser do
     do: throw{:list, pos}
 
   defp map(<<char>> <> data, pos, list)
-    when char in @whitespace,
+    when char in @whitespaces,
     do: map(data,  pos + 1, list)
-  defp map(<<@right_curly_bracket>> <> data, pos, list) do
+  defp map(<<?}>> <> data, pos, list) do
     result = for [value, key] <- Enum.chunk(list, 2), into: %{}, do: {key, value}
     {result, data, pos + 1}
   end
-  defp map(<<@comma>> <> _data, pos, []),
+  defp map(<<?,>> <> _data, pos, []),
     do: throw {:map, pos + 1}
-  defp map(<<@comma>> <> data, pos, list) do
+  defp map(<<?,>> <> data, pos, list) do
     {result, data, pos} = key(data, pos + 1)
     map(data, pos + 1, [result|list])
   end
-  defp map(<<@colon>> <> _data, pos, []),
+  defp map(<<?:>> <> _data, pos, []),
     do: throw {:map, pos + 1}
-  defp map(<<@colon>> <> data, pos, list) do
+  defp map(<<?:>> <> data, pos, list) do
     {result, data, pos} = json(data, pos + 1)
     map(data, pos + 1, [result|list])
   end
